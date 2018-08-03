1299c6db9deacb917beadd317968a4933d47ddf7
Guillermo Alvarez <guillermo@vaadin.com>
2018-08-03 11:43:17 +0300

    Add script to generate boms

---
+++ scripts/generateBoms.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd scripts/generator && npm install && cd ../../ && node scripts/generator/generate.js --platform=11.0-SNAPSHOT --versions=versions.json
+mkdir -p vaadin-bom
+cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
+mkdir -p vaadin-spring-bom
+cp scripts/generator/results/vaadin-spring-bom.xml vaadin-spring-bom/pom.xml
