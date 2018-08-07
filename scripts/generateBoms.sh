06409e3a74099e2ae70ed480ac3b8c93e280a6e6
Guille <alvarezguille@users.noreply.github.com>
2018-08-07 13:52:09 +0300

    Generate vaadin-spring-bom (#248) (#270)
    Fixes #261

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -2,3 +2,5 @@
 cd scripts/generator && npm install && cd ../../ && node scripts/generator/generate.js --platform=10.0-SNAPSHOT --versions=versions.json "$@"
 mkdir -p vaadin-bom
 cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
+mkdir -p vaadin-spring-bom
+cp scripts/generator/results/vaadin-spring-bom.xml vaadin-spring-bom/pom.xml
