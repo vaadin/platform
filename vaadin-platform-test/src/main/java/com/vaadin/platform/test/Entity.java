896522d1842b50c468cd6cd4cc0479a47a13f929
Alex <alex@vaadin.com>
2018-10-12 16:53:51 +0300

    Add test

---
+++ vaadin-platform-test/src/main/java/com/vaadin/platform/test/Entity.java
@@ -0,0 +1,21 @@
+package com.vaadin.platform.test;
+
+class Entity {
+	private String field;
+
+	Entity() {
+	}
+
+	Entity(String field) {
+		this.field = field;
+	}
+
+	String getField() {
+		return field;
+	}
+
+	void setField(String field)
+	{
+		this.field = field;
+	}
+}
