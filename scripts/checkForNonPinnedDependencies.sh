43a3a8778f222d9241a1524c02ff015552f08ae4
Tien Nguyen <nqtien.dev@gmail.com>
2018-10-24 12:42:07 +0300

    Improve non-pinned dependencies checker (#312)

--- scripts/checkForNonPinnedDependencies.sh
+++ scripts/checkForNonPinnedDependencies.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-if mvn dependency:tree | grep "version selected from constraint"
-then
-    echo "Error: Have found versions that are selected from constraint, pin those so they won't change themselves."
-    echo "Run mvn:dependency-tree to see all dependencies, including the ones marked as 'selected from constraint'"
-    exit 1
-else
-    echo "OK: No non-pinned dependencies"
-fi
+while read line
+do
+    echo "$line"
+    if [[ $line == *"version selected from constraint"* ]]; then
+        echo "Error: Have found versions that are selected from constraint, pin those so they won't change themselves."
+        echo "Run mvn:dependency-tree to see all dependencies, including the ones marked as 'selected from constraint'"
+        exit 1
+    fi
+done < <(mvn dependency:tree)
+echo "OK: No non-pinned dependencies"
