3b230bfc106e4a4f6b1707c739cc5e728d1471a5
Mehdi Javan <mehdi@vaadin.com>
2018-10-26 13:21:55 +0300

    Comments, typos and some refactorings after review

--- vaadin-platform-test/platform-test-root-context/src/main/java/com/vaadin/platform/osgi/StaticResourceServlet.java
+++ vaadin-platform-test/platform-test-root-context/src/main/java/com/vaadin/platform/osgi/StaticResourceServlet.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package flow.osgi;
+package com.vaadin.platform.osgi;
 
 import org.apache.commons.io.IOUtils;
 import org.osgi.service.http.HttpContext;
