87fcb3f98bbdd14edcd873b21e2f002742815dae
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-02-05 15:06:51 +0200

    Generate npm-shwinkwrap.json for vaadin-core as well (#531)
    * Generate npm-shwinkwrap.json for vaadin-core as well

--- scripts/npmShrinkwrap.sh
+++ scripts/npmShrinkwrap.sh
@@ -6,8 +6,18 @@ vaadinShrinkwrapNpmDir="$scriptDir"/tmp.npm.vaadin-shrinkwrap
 mkdir "$vaadinShrinkwrapNpmDir"
 cp "$scriptDir"/generator/results/vaadin-shrinkwrap-package.json "$vaadinShrinkwrapNpmDir"/package.json
 
+coreShrinkwrapNpmDir="$scriptDir"/tmp.npm.vaadin-core-shrinkwrap
+mkdir "$coreShrinkwrapNpmDir"
+cp "$scriptDir"/generator/results/vaadin-core-shrinkwrap-package.json "$coreShrinkwrapNpmDir"/package.json
+
+pushd $coreShrinkwrapNpmDir
+npm install
+npm shrinkwrap
+cp "$coreShrinkwrapNpmDir"/npm-shrinkwrap.json "$scriptDir"/generator/results/vaadin-core-npm-shrinkwrap.json
+popd
+
 pushd $vaadinShrinkwrapNpmDir
 npm install
 npm shrinkwrap
-cp "$vaadinShrinkwrapNpmDir"/npm-shrinkwrap.json "$scriptDir"/generator/results/npm-shrinkwrap.json
+cp "$vaadinShrinkwrapNpmDir"/npm-shrinkwrap.json "$scriptDir"/generator/results/vaadin-npm-shrinkwrap.json
 popd
