89263804cb6ffae50a9cbbcbc15d737dd8fbf325
Sayo Oladeji <sayo@vaadin.com>
2019-02-05 14:07:35 +0200

    Import Paragraph

--- vaadin-platform-test/src/main/java/com/vaadin/platform/test/ComponentsView.java
+++ vaadin-platform-test/src/main/java/com/vaadin/platform/test/ComponentsView.java
@@ -49,6 +49,7 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
