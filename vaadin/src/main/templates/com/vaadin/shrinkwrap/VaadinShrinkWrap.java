455f4f45b6444bbbd7eab8b169a46586db7e5604
Manolo Carrasco <manolo@apache.org>
2019-05-23 22:50:35 +0300

    Renave project version to match npm convention when annotating shrink

--- vaadin/src/main/templates/com/vaadin/shrinkwrap/VaadinShrinkWrap.java
+++ vaadin/src/main/templates/com/vaadin/shrinkwrap/VaadinShrinkWrap.java
@@ -20,6 +20,6 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 /**
  * Empty class which makes flow add npm vaadin-shrinkwrap dependency.
  */
-@NpmPackage(value = "@vaadin/vaadin-shrinkwrap", version = "${project.version}")
+@NpmPackage(value = "@vaadin/vaadin-shrinkwrap", version = "${shrink.version}")
 public class VaadinShrinkWrap {
 }
