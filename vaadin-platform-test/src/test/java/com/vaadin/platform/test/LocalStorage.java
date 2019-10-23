603092972c6d08bdbe303f617de7bf0f61bd0119
ZheSun88 <zhe@vaadin.com>
2019-10-23 15:42:48 +0300

    formatting

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/LocalStorage.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/LocalStorage.java
@@ -4,42 +4,42 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class LocalStorage {
-  private JavascriptExecutor js;
+    private JavascriptExecutor js;
 
-  public LocalStorage(WebDriver webDriver) {
-    this.js = (JavascriptExecutor) webDriver;
-  }
+    public LocalStorage(WebDriver webDriver) {
+        this.js = (JavascriptExecutor) webDriver;
+    }
 
-  public void removeItemFromLocalStorage(String item) {
-    js.executeScript(String.format(
-        "window.localStorage.removeItem('%s');", item));
-  }
+    public void removeItemFromLocalStorage(String item) {
+        js.executeScript(String.format(
+                "window.localStorage.removeItem('%s');", item));
+    }
 
-  public boolean isItemPresentInLocalStorage(String item) {
-    return !(js.executeScript(String.format(
-        "return window.localStorage.getItem('%s');", item)) == null);
-  }
+    public boolean isItemPresentInLocalStorage(String item) {
+        return !(js.executeScript(String.format(
+                "return window.localStorage.getItem('%s');", item)) == null);
+    }
 
-  public String getItemFromLocalStorage(String key) {
-    return (String) js.executeScript(String.format(
-        "return window.localStorage.getItem('%s');", key));
-  }
+    public String getItemFromLocalStorage(String key) {
+        return (String) js.executeScript(String.format(
+                "return window.localStorage.getItem('%s');", key));
+    }
 
-  public String getKeyFromLocalStorage(int key) {
-    return (String) js.executeScript(String.format(
-        "return window.localStorage.key('%s');", key));
-  }
+    public String getKeyFromLocalStorage(int key) {
+        return (String) js.executeScript(String.format(
+                "return window.localStorage.key('%s');", key));
+    }
 
-  public Long getLocalStorageLength() {
-    return (Long) js.executeScript("return window.localStorage.length;");
-  }
+    public Long getLocalStorageLength() {
+        return (Long) js.executeScript("return window.localStorage.length;");
+    }
 
-  public void setItemInLocalStorage(String item, String value) {
-    js.executeScript(String.format(
-        "window.localStorage.setItem('%s','%s');", item, value));
-  }
+    public void setItemInLocalStorage(String item, String value) {
+        js.executeScript(String.format(
+                "window.localStorage.setItem('%s','%s');", item, value));
+    }
 
-  public void clearLocalStorage() {
-    js.executeScript(String.format("window.localStorage.clear();"));
-  }
+    public void clearLocalStorage() {
+        js.executeScript(String.format("window.localStorage.clear();"));
+    }
 }
