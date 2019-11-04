72975662664096b1e0930d74e3dbd2656f97eb6a
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2019-11-04 13:51:39 +0200

    Create Production mode profile for further testing purpose (#945)
    * Create Production mode profile for further testing purpose
    
    * Remove left >
    
    * Correct travis
    
    * use /prod-mode/
    
    * remove production profile
    
    * Revert "remove production profile"
    
    This reverts commit 690105be37df55d50005d69279ec5c142249d531.

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
