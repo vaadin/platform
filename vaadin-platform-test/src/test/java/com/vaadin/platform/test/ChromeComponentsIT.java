ce161c611061b337935149c8ec90b866b5cc84fd
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2019-12-12 13:30:27 +0200

    open combobox popup

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
@@ -116,7 +116,8 @@ public class ChromeComponentsIT extends ParallelTest {
                 .id("input");
         assertElementRendered(textField);
 
-        comboBox.$(TestBenchElement.class).id("toggleButton").click();
+        comboBox.openPopup();
+        //comboBox.$(TestBenchElement.class).id("toggleButton").click();
 
         WebElement dropDown = $("vaadin-combo-box-overlay").id("overlay");
 
