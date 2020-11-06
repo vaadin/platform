3f467e7f32af18c5cc7d06d8d5f77866a44aabea
Manolo Carrasco <manolo@vaadin.com>
2020-11-06 09:33:27 +0100

    re-factor: compute version/snapshot from pom instead of hardcoding or passing it

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-cd scripts/generator && npm install && cd ../../ && node scripts/generator/generate.js --platform=17.0-SNAPSHOT --versions=versions.json "$@"
+
+# use platform version  from the root pom.xml
+version=`mvn -N help:evaluate -Dexpression=project.version -q -DforceStdout`
+# if platform version is a SNAPSHOT, use SNAPSHOTS for other dependencies
+expr "$version" : '[0-9]*.[0-9]*-SNAPSHOT' >/dev/null && snapshot=--useSnapshots
+
+# install npm deps needed for the generator node script
+[ ! -d scripts/generator/node_modules ] && (cd scripts/generator && npm install)
+
+# run the generator
+cmd="node scripts/generator/generate.js --platform=$version --versions=versions.json $snapshot"
+echo Running: "$cmd"
+$cmd || exit 1
+
+# copy generated poms to the final place
 mkdir -p vaadin-bom
 cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
 mkdir -p vaadin-spring-bom
