867fc1313ac438bc6a6ae663bd84b1bc1ae9cc6e
ZheSun88 <zhe@vaadin.com>
2021-02-09 09:04:03 +0200

    chore: Align bom generation in 10.0 branch

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-cd scripts/generator && npm install && cd ../../ && node scripts/generator/generate.js --platform=10.0-SNAPSHOT --versions=versions.json "$@"
+# use platform version  from the root pom.xml
+version=`mvn -N help:evaluate -Dexpression=project.version -q -DforceStdout`
+snapshot=$1
+
+# install npm deps needed for the generator node script
+[ ! -d scripts/generator/node_modules ] && (cd scripts/generator && npm install)
+
+# run the generator
+cmd="node scripts/generator/generate.js --platform=$version --versions=versions.json $snapshot"
+echo Running: "$cmd"
+$cmd || exit 1
+
+# copy generated poms to the final place
 mkdir -p vaadin-bom
 cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
 mkdir -p vaadin-spring-bom
