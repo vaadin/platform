a707ed00efdc3d942f0dfc86d0a66f8c9437d402
ZheSun88 <zhe@vaadin.com>
2021-03-02 12:11:19 +0200

    fix: remove flow-maven-plugin from bom

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -18,3 +18,5 @@ mkdir -p vaadin-bom
 cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
 mkdir -p vaadin-spring-bom
 cp scripts/generator/results/vaadin-spring-bom.xml vaadin-spring-bom/pom.xml
+
+cp scripts/generator/results/vaadin-maven-plugin-pom.xml vaadin-maven-plugin/pom.xml
