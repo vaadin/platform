a71f78eaf0c0b9799d2813d4c9c1163022f87b8c
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-03-02 18:57:15 +0200

    test: add test for global css loader (#1872)
    * test: add test for global css loader
    
    * verify color is set via global css file
    
    Co-authored-by: Manuel Carrasco Moñino <manolo@vaadin.com>

--- vaadin-platform-hybrid-test/frontend/views/helloworldts/hello-world-ts-view.ts
+++ vaadin-platform-hybrid-test/frontend/views/helloworldts/hello-world-ts-view.ts
@@ -3,6 +3,7 @@ import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-text-field';
 import { customElement, html } from 'lit-element';
 import { View } from '../view';
+import './hello-world-ts-view.global.css';
 
 @customElement('hello-world-ts-view')
 export class HelloWorldTSView extends View {
@@ -10,6 +11,7 @@ export class HelloWorldTSView extends View {
 
   render() {
     return html`
+      <div>Hello World</div>
       <vaadin-text-field label="Your name" @value-changed="${this.nameChanged}"></vaadin-text-field>
       <vaadin-button @click="${this.sayHello}">Say hello</vaadin-button>
     `;
