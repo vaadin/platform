48f607ac0b507d95c0093adc75cac28d6108a0cf
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2021-04-13 09:05:57 +0300

    chore: fix cherry pick script function

--- scripts/cherryPick.js
+++ scripts/cherryPick.js
@@ -108,7 +108,7 @@ async function cherryPickCommits(){
     } catch (err) {
       console.error(`Cannot Pick the Commit:${arrSHA[i]} to ${arrBranch[i]}, error :${err}`);
       await labelCommit(arrURL[i], `need to pick manually  ${arrBranch[i]}`);
-      await postComment(arrURL[i], arrUser[i], arrBranch[i], ${err})；
+      await postComment(arrURL[i], arrUser[i], arrBranch[i])；
       
       await exec(`git cherry-pick --abort`);
       await exec(`git checkout master`);
