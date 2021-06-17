593113db2031fa0245b2e1474bbd48e8953fdb36
caalador <mikael.grankvist@vaadin.com>
2021-06-17 12:57:22 +0300

    feat: Add generation of jandex index (#2197)
    * feat: Add generation of jandex index
    
    Generate jandex index package for
    vaadin and vaadin-core.
    
    Closes vaadin/flow#9862
    
    * Add test for jandex modules

---
+++ vaadin-jandex/src/test/java/com/vaadin/jandex/JandexSmokeTest.java
@@ -0,0 +1,46 @@
+package com.vaadin.jandex;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.Index;
+import org.jboss.jandex.IndexReader;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.component.charts.model.ChartConfiguration;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.HandlerHelper;
+
+public class JandexSmokeTest {
+
+    @Test
+    public void generatedJandex_containsContentFromFlowAndComponents()
+            throws IOException {
+        try (InputStream jandexStream = JandexSmokeTest.class.getClassLoader()
+                .getResourceAsStream("META-INF/jandex.idx")) {
+            IndexReader reader = new IndexReader(jandexStream);
+            final Index index = reader.read();
+
+            ClassInfo classByName = index.getClassByName(
+                    DotName.createSimple(HandlerHelper.class.getName()));
+            Assert.assertNotNull("Class from flow-server was not found",
+                    classByName);
+
+            classByName = index.getClassByName(
+                    DotName.createSimple(VerticalLayout.class.getName()));
+            Assert.assertNotNull("Class from a component was not found",
+                    classByName);
+
+            classByName = index.getClassByName(
+                    DotName.createSimple(ChartConfiguration.class.getName()));
+            Assert.assertNotNull("Class from commercial component was missing",
+                    classByName);
+
+        }
+
+    }
+
+}
