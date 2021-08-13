33ec91ecb8bd4de4953bbc846b6a49cd7efbf6a7
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2021-08-13 09:50:25 +0300

    Update ComponentsIT.java

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
@@ -35,7 +35,7 @@ public class ComponentsIT extends ParallelTest {
         String browsers = System.getProperty("grid.browsers");
         if (sauceUser != null && !sauceUser.isEmpty()) {
             if (browsers == null || browsers.isEmpty()) {
-                Parameters.setGridBrowsers("ie11,firefox,chrome,safari-9,safari-10,safari,edge,edge-18");
+                Parameters.setGridBrowsers("ie11,firefox,chrome,safari-9,safari-10,safari-14,edge,edge-18");
             } else {
                 Parameters.setGridBrowsers(browsers);
             }
