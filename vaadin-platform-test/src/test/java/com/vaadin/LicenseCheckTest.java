2746e815658894a513a75dfc34fd556909dce4bb
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2022-01-28 11:38:06 +0200

    Add Vaadin Quarkus Extention (#2698)
    * Add Vaadin Quarkus Extention
    
    * whitelist apache
    
    * use vaadin-bom instead of vaadin-spring-bom

--- vaadin-platform-test/src/test/java/com/vaadin/LicenseCheckTest.java
+++ vaadin-platform-test/src/test/java/com/vaadin/LicenseCheckTest.java
@@ -54,6 +54,9 @@ public class LicenseCheckTest {
         whitelist.add("http://www.apache.org/licenses/LICENSE-2.0.html");
         whitelist.add("https://www.apache.org/licenses/LICENSE-2.0.txt");
         whitelist.add("https://spdx.org/licenses/Apache-2.0#licenseText");
+        whitelist.add("https://www.apache.org/licenses/LICENSE-2.0.html");
+        whitelist.add("http://repository.jboss.org/licenses/apache-2.0.txt");
+        whitelist.add("https://repository.jboss.org/licenses/apache-2.0.txt");
          /*
          * License names used by some projects that define their license to be
          * something like to http://projectdomain.com/license, for which the
@@ -88,6 +91,12 @@ public class LicenseCheckTest {
 
         // aopalliance:aopalliance
         whitelist.add("Public Domain");
+        
+        // Universal Permissive License
+        whitelist.add("http://opensource.org/licenses/UPL");
+        
+        // BSD-3-Clause
+        whitelist.add("https://asm.ow2.io/license.html");
     }
 
     private static final List<String> excludeDirs = Arrays.asList(".git", "bower_components", "node", "node_modules",
