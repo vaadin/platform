80c37e781f3e6047e8abae6eb1b5b1f3c81ef197
Zhe Sun <zhe@vaadin.com>
2022-03-03 13:08:19 +0200

    chore: add wait for action

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
@@ -41,8 +41,14 @@ public class ComponentsIT extends AbstractPlatformTest {
     HashMap<String, Runnable> beforeRunsByTag = new HashMap<String, Runnable>() {
         private static final long serialVersionUID = 1L;
         {
-            put("com.vaadin.flow.component.grid.contextmenu.GridContextMenu", () -> $(GridElement.class).first().getCell(1, 0).click());
-            put("com.vaadin.flow.component.grid.contextmenu.GridMenuItem", () -> $(GridElement.class).first().getCell(1, 0).click());
+            put("com.vaadin.flow.component.grid.contextmenu.GridContextMenu", () -> {
+                $(GridElement.class).first().getCell(1, 0).click();
+                waitUntil(driver->$("vaadin-context-menu").exists());
+            });
+            put("com.vaadin.flow.component.grid.contextmenu.GridMenuItem", () -> {
+                $(GridElement.class).first().getCell(1, 0).click();
+                waitUntil(driver->$("vaadin-context-menu").exists());
+            });
             put("vaadin-confirm-dialog", () -> $(ButtonElement.class).id("open-confirm-dialog").click());
             put("vaadin-dialog", () -> $(ButtonElement.class).id("open-dialog").click());
             put("vaadin-login-overlay", () -> $(ButtonElement.class).id("open-login-overlay").click());
