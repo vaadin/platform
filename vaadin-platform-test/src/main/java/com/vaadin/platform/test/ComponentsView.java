5505a0d4d3bbe387ce92fb58a4159de50216e198
Zhe Sun <zhe@vaadin.com>
2022-05-30 16:14:56 +0300

    correct import

--- vaadin-platform-test/src/main/java/com/vaadin/platform/test/ComponentsView.java
+++ vaadin-platform-test/src/main/java/com/vaadin/platform/test/ComponentsView.java
@@ -17,7 +17,12 @@ package com.vaadin.platform.test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
