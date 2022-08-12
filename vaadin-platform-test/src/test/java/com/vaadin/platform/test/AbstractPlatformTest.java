56d4d4207d6a8d783c1f6b720fab208d8cfe2b62
Manolo Carrasco <manolo@vaadin.com>
2022-08-12 08:16:41 +0200

    chore: not need to use localhost, local address works for sauce

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/AbstractPlatformTest.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/AbstractPlatformTest.java
@@ -32,7 +32,7 @@ public abstract class AbstractPlatformTest extends ParallelTest {
     public static final int SERVER_PORT = Integer
             .parseInt(System.getProperty("serverPort", "8080"));
 
-    static String hostName;
+    static final String hostName = IPAddress.findSiteLocalAddress();
     static boolean isSauce;
     static boolean isHub;
     static boolean isLocal;
@@ -57,7 +57,6 @@ public abstract class AbstractPlatformTest extends ParallelTest {
                 WebDriverManager.chromedriver().setup();
             }
         }
-        hostName = isHub ? IPAddress.findSiteLocalAddress() : "localhost";
         getLogger().info("Running Tests app-url=http://{}:{} mode={}", hostName,
                 SERVER_PORT,
                 isSauce ? "SAUCE (user:" + sauceUser + ")"
