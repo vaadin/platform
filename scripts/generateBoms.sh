bf723414ec57448f777d9b8f6c6ece31add5e069
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2022-11-21 09:38:42 +0100

    feat: add hilla-react module to 23.3 (#3468)
    * feat: add hilla-react module
    
    * Propagate changes made in main versions file
    
    * Generate version.json for Hilla and Hilla-React
    
    * Use versions from BOM in Hilla POMs

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -28,4 +28,5 @@ cp scripts/generator/results/vaadin-gradle-plugin-portal-pom.xml vaadin-gradle-p
 cp scripts/generator/results/vaadin-platform-servlet-containers-tests-pom.xml vaadin-platform-servlet-containers-tests/pom.xml
 cp scripts/generator/results/vaadin-core-versions.json vaadin-core/vaadin-core-versions.json
 cp scripts/generator/results/vaadin-versions.json vaadin/vaadin-versions.json
-
+cp scripts/generator/results/hilla-versions.json hilla/hilla-versions.json
+cp scripts/generator/results/hilla-react-versions.json hilla-react/hilla-react-versions.json
