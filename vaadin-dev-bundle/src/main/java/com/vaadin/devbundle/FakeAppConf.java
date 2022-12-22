6ffc9e2613903f3f2032e14442f06ed5c84f04c0
Artur Signell <artur@vaadin.com>
2022-12-22 14:18:32 +0200

    Add line-awesome assets

---
+++ vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -0,0 +1,9 @@
+package com.vaadin.devbundle;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("vaadin-dev-bundle")
+public class FakeAppConf implements AppShellConfigurator{
+    
+}
