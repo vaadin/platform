358d0b041231f5e32e4a891080764932561e24e4
Manolo Carrasco <manolo@vaadin.com>
2023-01-16 10:38:46 +0100

    align with master

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -286,7 +286,7 @@ async function main() {
   fs.existsSync('package.json') && fs.unlinkSync('package.json');
 
   await run('mvn package -ntp -B -Pproduction -DskipTests -q');
-  await run('mvn dependency:tree -Dscope=compile -ntp -B', { output: 'target/tree-maven.txt' });
+  await run('mvn dependency:tree -ntp -B', { output: 'target/tree-maven.txt' });
   await run('mvn -ntp -B org.cyclonedx:cyclonedx-maven-plugin:makeAggregateBom -q');
   await run('npm ls --depth 6', { output: 'target/tree-npm.txt' });
 
