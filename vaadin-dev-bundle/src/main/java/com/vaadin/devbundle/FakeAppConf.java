1e0c48e7cc060b16b46bd7d0f7a73f8c1f2ab243
Mikhail Shabarov <mikhail@vaadin.com>
2023-02-08 12:43:42 +0200

    fix: Add missing npm packages to the dev bundle

--- vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -9,6 +9,14 @@ import com.vaadin.flow.theme.Theme;
 @JsModule("@vaadin-component-factory/vcf-nav")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@NpmPackage(value = "@vaadin/bundles", version = "%bundles.version%")
+@NpmPackage(value = "@vaadin/component-base", version = "%component.base.version%")
+@NpmPackage(value = "@vaadin/field-base", version = "%field.base.version%")
+@NpmPackage(value = "@vaadin/input-container", version = "%input.container.version%")
+@NpmPackage(value = "@vaadin/lit-renderer", version = "%lit.renderer.version%")
+@NpmPackage(value = "@vaadin/overlay", version = "%overlay.version%")
+@NpmPackage(value = "@vaadin/vaadin-development-mode-detector", version = "%devmode.detector.version%")
+@NpmPackage(value = "@vaadin/vaadin-usage-statistics", version = "%statistics.version%")
 public class FakeAppConf implements AppShellConfigurator{
 
 }
