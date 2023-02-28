b5cd515be7e457f144847f97f38970e010314e94
Artur Signell <artur@vaadin.com>
2023-02-28 18:16:48 +0200

    simpl

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ComponentsIT.java
@@ -1,22 +1,10 @@
 package com.vaadin.platform.test;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 import org.junit.Test;
 
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.flow.component.grid.testbench.GridElement;
-import com.vaadin.flow.component.html.testbench.DivElement;
 import com.vaadin.flow.component.notification.testbench.NotificationElement;
-import com.vaadin.testbench.ElementQuery;
 import com.vaadin.testbench.Parameters;
-import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.parallel.SauceLabsIntegration;
-
 
 public class ComponentsIT extends AbstractPlatformTest {
 
@@ -24,7 +12,7 @@ public class ComponentsIT extends AbstractPlatformTest {
         if (SauceLabsIntegration.isConfiguredForSauceLabs()) {
             String browsers = System.getProperty("grid.browsers");
             if (browsers == null || browsers.isEmpty()) {
-                Parameters.setGridBrowsers("firefox,safari,edge");
+                Parameters.setGridBrowsers("safari");
             } else {
                 Parameters.setGridBrowsers(browsers);
             }
@@ -36,38 +24,8 @@ public class ComponentsIT extends AbstractPlatformTest {
         return "/prod-mode/";
     }
 
-
-    HashMap<String, Runnable> beforeRunsByTag = new HashMap<String, Runnable>() {
-        private static final long serialVersionUID = 1L;
-        {
-            put("com.vaadin.flow.component.grid.contextmenu.GridContextMenu", () -> {
-                $(GridElement.class).first().getCell(1, 0).click();
-                waitUntil(driver->$("vaadin-context-menu").exists());
-            });
-            put("com.vaadin.flow.component.grid.contextmenu.GridMenuItem", () -> {
-                $(GridElement.class).first().getCell(1, 0).click();
-                waitUntil(driver->$("vaadin-context-menu").exists());
-            });
-            put("vaadin-confirm-dialog", () -> $(ButtonElement.class).id("open-confirm-dialog").click());
-            put("vaadin-dialog", () -> $(ButtonElement.class).id("open-dialog").click());
-            put("vaadin-login-overlay", () -> $(ButtonElement.class).id("open-login-overlay").click());
-            put("vaadin-context-menu", () -> $(DivElement.class).id("context-menu-target").click());
-            put("vaadin-context-menu-item", () -> $(DivElement.class).id("context-menu-target").click());
-        }
-    };
-
-    List<String> excludeComponents = Arrays.asList("vaadin-message");
-
     @Test
     public void appWorks() throws Exception {
         $(NotificationElement.class).waitForFirst();
-    }
-
-    private <T extends TestBenchElement> void checkElement(ElementQuery<T> $) {
-        assertTrue($.exists());
-        String tagName = $.first().getTagName().toLowerCase();
-        if (tagName.contains("-")) {
-            assertTrue((Boolean) executeScript("return !!window.customElements.get(arguments[0])", tagName));
-        }
     }
 }
