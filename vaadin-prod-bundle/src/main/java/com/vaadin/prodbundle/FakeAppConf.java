53728e7861db6ef18526232e6e00874f550249cc
Mikhail Shabarov <mikhail@vaadin.com>
2023-04-18 13:33:05 +0300

    feat: Add default production bundle module
    This adds a standalone maven module for default prod bundle.
    The bundle is generated in a regular way with 'build-frontend' goal.
    Will be used by Flow in case if an app has no frontend add-ons.
    
    Part-of https://github.com/vaadin/platform/issues/4046

---
+++ vaadin-prod-bundle/src/main/java/com/vaadin/prodbundle/FakeAppConf.java
@@ -0,0 +1,16 @@
+package com.vaadin.prodbundle;
+
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("vaadin-prod-bundle")
+@PWA(name = "vaadin-prod-bundle", shortName = "vaadin-prod-bundle")
+@JsModule("@vaadin-component-factory/vcf-nav")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+public class FakeAppConf implements AppShellConfigurator{
+
+}
