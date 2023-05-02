738d04c3af26948ae79631240919b12edd35959a
Artur <artur@vaadin.com>
2023-05-02 15:29:23 +0300

    Update FakeAppConf.java

--- vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -1,7 +1,5 @@
 package com.vaadin.devbundle;
 
-import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
