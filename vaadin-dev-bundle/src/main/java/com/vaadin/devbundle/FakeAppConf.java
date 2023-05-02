3c28066ab8b03c263b9ae73d6d06e9d0014f6dff
Artur <artur@vaadin.com>
2023-05-02 15:24:27 +0300

    chore!: Remove external modules from dev bundle
    Having external dependencies without a managed the version makes it impossible to upgrade those dependencies. We already remove used of line-awesome as an npm dependency in 24.0 and vcf-nav is being replaced in 24.1 with `vaadin-side-nav`

--- vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ vaadin-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -9,8 +9,6 @@ import com.vaadin.flow.theme.Theme;
 @Theme("vaadin-dev-bundle")
 @PWA(name = "vaadin-dev-bundle", shortName = "vaadin-dev-bundle")
 @JsModule("@vaadin-component-factory/vcf-nav")
-@NpmPackage(value = "line-awesome", version = "1.3.0")
-@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 public class FakeAppConf implements AppShellConfigurator{
 
 }
