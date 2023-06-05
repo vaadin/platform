e70e70410e1fc629a8ad39d23fd7c2a697a6d0af
caalador <mikael.grankvist@vaadin.com>
2023-06-05 08:52:47 +0300

    fix: update plugin versions (#4210)
    * fix: update plugin versions
    
    Update plugin versions used
    in vaadin-maven-plugin.
    
    Fixes vaadin/flow#16881
    
    * Update whitelist

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -42,7 +42,7 @@ const licenseWhiteList = [
 
 const cveWhiteList = {
   // Remove when https://github.com/jeremylong/DependencyCheck/pull/5415
-  'pkg:maven/com.google.guava/guava@31.1-jre': ['CVE-2020-8908']
+  'pkg:maven/com.google.guava/guava@31.1-jre': ['CVE-2020-8908', 'CVE-2023-2976']
 }
 
 const STYLE = `<style>
