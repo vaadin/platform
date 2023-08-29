fb0137218c6c8e80286e9489ba46493c6770c357
Zhe Sun <zhe@vaadin.com>
2023-08-29 10:46:59 +0300

    chore: remove hilla module and related stuff

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -16,8 +16,6 @@ $cmd || exit 1
 # copy generated poms to the final place
 mkdir -p vaadin-bom
 cp scripts/generator/results/vaadin-bom.xml vaadin-bom/pom.xml
-mkdir -p hilla-bom
-cp scripts/generator/results/hilla-bom.xml hilla-bom/pom.xml
 mkdir -p vaadin-spring-bom
 cp scripts/generator/results/vaadin-spring-bom.xml vaadin-spring-bom/pom.xml
 mkdir -p vaadin-platform-javadoc
@@ -30,8 +28,6 @@ cp scripts/generator/results/vaadin-core-versions.json vaadin-core-internal/vaad
 cp scripts/generator/results/vaadin-versions.json vaadin-internal/vaadin-versions.json
 cp scripts/generator/results/vaadin-dev-bundle-pom.xml vaadin-dev-bundle/pom.xml
 cp scripts/generator/results/vaadin-prod-bundle-pom.xml vaadin-prod-bundle/pom.xml
-cp scripts/generator/results/hilla-versions.json hilla/hilla-versions.json
-cp scripts/generator/results/hilla-react-versions.json hilla-react/hilla-react-versions.json
 
 cat vaadin-prod-bundle/pom.xml|perl -p -e "s~vaadin-prod-bundle~vaadin-prod-bundle-unoptimized~g"|perl -p -e "s~Vaadin Prod Bundle~Vaadin Prod Bundle Unoptimized~g"|perl -p -e "s~<optimizeBundle>true</optimizeBundle>~<optimizeBundle>false</optimizeBundle>~g"|perl -p -e "s~<skipTests>false</skipTests>~<skipTests>true</skipTests>~g" > vaadin-prod-bundle/pom-unoptimized.xml
 perl -pi -e 's~</dependencies>~<dependency><groupId>com.vaadin</groupId><artifactId>vaadin-prod-bundle-unoptimized</artifactId><version>\${project.version}</version><optional>true</optional><scope>test</scope></dependency></dependencies>~'  vaadin-prod-bundle/pom.xml
