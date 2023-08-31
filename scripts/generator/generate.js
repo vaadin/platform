6d185ff4213b8f793e88095e69b4e962f0f89dca
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2023-08-31 11:02:35 +0300

    chore: remove hilla module and related stuff (#4431)

--- scripts/generator/generate.js
+++ scripts/generator/generate.js
@@ -34,12 +34,6 @@ const vaadinCoreJsonFileName = getResultsFilePath('vaadin-core-versions.json');
 const vaadinJsonTemplateFileName = getTemplateFilePath('template-vaadin-versions.json');
 const vaadinJsonResultFileName = getResultsFilePath('vaadin-versions.json');
 
-const hillaJsonTemplateFileName = getTemplateFilePath('template-hilla-versions.json');
-const hillaJsonResultFileName = getResultsFilePath('hilla-versions.json');
-
-const hillaReactJsonTemplateFileName = getTemplateFilePath('template-hilla-react-versions.json');
-const hillaReactJsonResultFileName = getResultsFilePath('hilla-react-versions.json');
-
 const corePackageTemplateFileName = getTemplateFilePath('template-vaadin-core-package.json');
 const corePackageResultFileName = getResultsFilePath('vaadin-core-package.json');
 
@@ -75,9 +69,6 @@ const platformJavadocResultPomFileName = getResultsFilePath('vaadin-platform-jav
 
 const servletContainersTestsPomFileName = getTemplateFilePath('template-servlet-containers-tests-pom.xml');
 
-const mavenHillaBomTemplateFileName = getTemplateFilePath('template-hilla-bom.xml');
-const mavenHillaBomResultFileName = getResultsFilePath('hilla-bom.xml');
-
 const devBundleTemplateFileName = getTemplateFilePath('template-dev-bundle-pom.xml');
 const devBundlePomResultFileName = getResultsFilePath('vaadin-dev-bundle-pom.xml');
 
@@ -96,23 +87,10 @@ writer.writeSeparateJson(versions.core, coreJsonTemplateFileName, vaadinCoreJson
 writer.writeSeparateJson(versions.platform, coreJsonTemplateFileName, vaadinCoreJsonFileName, "platform");
 writer.writeSeparateJson(versions.vaadin, vaadinJsonTemplateFileName, vaadinJsonResultFileName, "vaadin");
 writer.writeSeparateJson(versions.platform, vaadinJsonTemplateFileName, vaadinJsonResultFileName, "platform");
-writer.writeSeparateJson(versions.bundles, hillaJsonTemplateFileName, hillaJsonResultFileName, "bundles");
-writer.writeSeparateJson(versions.core, hillaJsonTemplateFileName, hillaJsonResultFileName, "core");
-writer.writeSeparateJson(versions.vaadin, hillaJsonTemplateFileName, hillaJsonResultFileName, "vaadin");
-writer.writeSeparateJson(versions.bundles, hillaReactJsonTemplateFileName, hillaReactJsonResultFileName, "bundles");
-writer.writeSeparateJson(versions.react, hillaReactJsonTemplateFileName, hillaReactJsonResultFileName, "react");
-
-const hilla = process.env.HILLA || platform.replace(/^24/, 2);
-versions.core.hilla = {javaVersion: hilla};
-// write hilla version to hilla-react-versions.json as platform
-const hillaVersions = {platform : hilla};
-writer.writeSeparateJson(hillaVersions.platform, hillaJsonTemplateFileName, hillaJsonResultFileName, "platform");
-writer.writeSeparateJson(hillaVersions.platform, hillaReactJsonTemplateFileName, hillaReactJsonResultFileName, "platform");
 
 writer.writePackageJson(versions.core, corePackageTemplateFileName, corePackageResultFileName);
 writer.writePackageJson(versions.vaadin, vaadinPackageTemplateFileName, vaadinPackageResultFileName);
 writer.writeMaven(versions, mavenBomTemplateFileName, mavenBomResultFileName);
-writer.writeMaven(versions, mavenHillaBomTemplateFileName, mavenHillaBomResultFileName);
 writer.writeMaven(versions, mavenSpringBomTemplateFileName, mavenSpringBomResultFileName);
 writer.writeReleaseNotes(versions, releaseNotesTemplateFileName, releaseNotesResultFileName);
 writer.writeReleaseNotes(versions, releaseNotesMaintenanceTemplateFileName, releaseNotesMaintenanceResultFileName);
