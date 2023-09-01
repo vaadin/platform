fb884173291e577be7959b64b5c6e225514a4329
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2023-09-01 14:35:26 +0300

    add FP to SBOM check file (24.0) [skip ci] (#4444)
    * add FP to SBOM check file (24.0)
    
    * Update generateAndCheckSBOM.js
    
    * chore: exclude tomcat vulnerable version
    
    * fix spring-boot vulnerability
    
    * one more tomcat dependency

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -44,7 +44,9 @@ const cveWhiteList = {
   // Check fix in vaadin-testbench/pom.xml, and update when Selenium is fixed
   // 'pkg:maven/com.google.guava/guava@31.1-jre': ['CVE-2020-8908', 'CVE-2023-2976']
   // based on the issue this is not a CVE https://github.com/FasterXML/jackson-databind/issues/3972
-  'pkg:maven/com.fasterxml.jackson.core/jackson-databind@2.14.2' : ['CVE-2023-35116']
+  'pkg:maven/com.fasterxml.jackson.core/jackson-databind@2.14.2' : ['CVE-2023-35116'],
+  // exclude false positive based on https://github.com/jeremylong/DependencyCheck/issues/5912
+  'pkg:maven/io.netty/netty-handler@4.1.94.Final': ['CVE-2023-4586']
 }
 
 const STYLE = `<style>
