1dbd03f5f92ce03e7a4e5e1ae480a16686b0d28d
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2023-09-21 12:21:20 +0300

    chore: bump spring boot to 2.7.16 (#4493)
    * chore: bump spring boot to 2.7.16
    
    * Update generateAndCheckSBOM.js

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -41,7 +41,7 @@ const licenseWhiteList = [
 ];
 
 const cveWhiteList = {
-  'pkg:maven/org.springframework/spring-web@5.3.29' : ['CVE-2016-1000027'],
+  'pkg:maven/org.springframework/spring-web@5.3.30' : ['CVE-2016-1000027'],
   // based on the issue this is not a CVE https://github.com/FasterXML/jackson-databind/issues/3972
   'pkg:maven/com.fasterxml.jackson.core/jackson-databind@2.14.2' : ['CVE-2023-35116'],
   'pkg:maven/com.vaadin/vaadin-core@23.3-SNAPSHOT' : ['CVE-2023-25499', 'CVE-2023-25500']
