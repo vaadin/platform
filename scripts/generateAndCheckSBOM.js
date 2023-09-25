d0e8959285de6082cba2672d5732462437244402
Manuel Carrasco Moñino <manolo@vaadin.com>
2023-09-25 16:44:50 +0200

    add BlueOak-1.0.0 to the whitelist

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -41,7 +41,8 @@ const licenseWhiteList = [
   'http://oss.sgi.com/projects/FreeB',
   'Apache-1.1',
   'http://font.ubuntu.com/ufl/ubuntu-font-licence-1.0.txt',
-  'http://www.spdx.org/licenses/BSD-4-Clause'
+  'http://www.spdx.org/licenses/BSD-4-Clause',
+  'BlueOak-1.0.0'
 ];
 
 const cveWhiteList = {
