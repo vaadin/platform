34cfaadef2c97f09e17d7c8ac6703457022b5ee4
Tomi Virtanen <tltv@vaadin.com>
2024-02-05 16:53:02 +0200

    chore: wait for dev bundle to be ready

--- vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
+++ vaadin-platform-test/src/test/java/com/vaadin/platform/test/ChromeComponentsIT.java
@@ -83,8 +83,13 @@ public class ChromeComponentsIT extends AbstractPlatformTest {
     }
 
     @Before
-    public void load(){
-        $(NotificationElement.class).waitForFirst();
+    public void load() {
+        if (getDriver().getPageSource().contains("<div class=\"message\">Building front-end development bundle</div>")) {
+            // Wait for the development bundle to be loaded
+            $(NotificationElement.class).waitForFirst(180);
+        } else {
+            $(NotificationElement.class).waitForFirst();
+        }
     }
 
 
