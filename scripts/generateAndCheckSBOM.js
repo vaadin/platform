56e26f0bad45b72b5b912912c890b70cd802de8e
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2024-02-29 11:19:24 +0200

    chore: suppress one report from our tool [skip ci]

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -56,6 +56,10 @@ const cveWhiteList = {
   'pkg:maven/me.friwi/jcef-api@jcef-af53d63%2Bcef-104.4.23%2Bg46ae630%2Bchromium-104.0.5112.102' : {
     cves: ['CVE-2024-21639', 'CVE-2024-21640'],
     description: 'Wait for the update from the jcefmaven community. Meanwhile the swing-kit is supposed to be used with fixed websites and not to browse the internet, we have a check for that, so the only possible attacker would be the same person that created the swing application, aka our customer devs. so this vulnerability is not classified by us as critical issue'
+  },
+  'pkg:npm/railroad-diagrams@1.0.0' : {
+    cves: ['CVE-2024-26467'],
+    description: 'This is coming from the tools, @cyclonedx/cyclonedx-npm, we have used for sbom module, FP for us. '
   }
 }
 
