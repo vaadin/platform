a61392e7df6e6756e6405c155a55e16a84055072
caalador <mikael.grankvist@vaadin.com>
2024-10-24 14:32:32 +0300

    test: Add react mixed test module (#6865)
    * feat: Add react mixed test module
    
    Add a mixed React Hilla Flow
    test module to test interoperability
    of the 2 features
    
    Fixes #4972
    
    * update to new 24.6
    
    * react in name and description
    
    * fix application configuration packages.
    
    * Use abstractPlatformTest for test base
    
    * Remove log, fix test
    
    * Flow to hilla navigation.
    
    * fix menu find
    
    * Add some navigation tests.
    
    * Update with navigation tests.
    
    * remove name fusion and add react instead
    
    * Add Dashboard component
    
    * remove index to not get issues with flow route ´flow´
    
    * module only behind profile
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

---
+++ vaadin-platform-react-hybrid-test/src/main/frontend/views/hilla/@index.tsx
@@ -0,0 +1,19 @@
+import type {ViewConfig} from "@vaadin/hilla-file-router/types.js";
+import { NavLink } from "react-router-dom";
+
+export const config: ViewConfig = {
+    menu: {
+        exclude: true,
+        title: "ERROR!",
+    },
+};
+
+export default function Hilla() {
+    return (
+        <div>
+            <span id={"hilla"} hidden={true}></span>
+            <div>"Hilla root view for menu!"</div>
+            <NavLink to={"/hilla/hello-react"} id={"toHello"}>To hello react</NavLink>
+        </div>
+    );
+}
