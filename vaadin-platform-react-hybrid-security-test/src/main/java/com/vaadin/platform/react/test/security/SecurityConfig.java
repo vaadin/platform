6ce0151a1a1f7a2b0a6713486a1f1f2507c0f835
caalador <mikael.grankvist@vaadin.com>
2024-10-24 15:03:06 +0300

    test: Flow-Hilla with security module (#6899)
    * feat: Flow-Hilla with security module
    
    Add test module for Flow-Hilla
    mixed application with security
    configuration.
    
    part of #4972
    
    * Add views with roles.
    
    Add tests for with and without login
    for both hilla and flow main layout
    
    * add security module
    
    * rename profile
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

---
+++ vaadin-platform-react-hybrid-security-test/src/main/java/com/vaadin/platform/react/test/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.vaadin.platform.react.test.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.hilla.route.RouteUtil;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+    private final RouteUtil routeUtil;
+
+    public SecurityConfig(RouteUtil routeUtil) {
+        this.routeUtil = routeUtil;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(registry -> {
+            registry.requestMatchers(new AntPathRequestMatcher("/")).permitAll();
+            registry.requestMatchers(routeUtil::isRouteAllowed).permitAll();
+        });
+        super.configure(http);
+        setLoginView(http, "/login", "/");
+    }
+
+    @Override
+    protected void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().requestMatchers(new AntPathRequestMatcher("/images/**"));
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}user")
+                .roles(Role.USER)
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles(Role.ADMIN)
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
