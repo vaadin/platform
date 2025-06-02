3d6efe2a315e23a9a7884f9d4382dc869374446b
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-06-02 13:36:22 +0300

    chore: upgrade sbom creation script [skip ci] (#7683)
    fixes https://github.com/vaadin/platform/issues/7631

--- scripts/generateAndCheckSBOM.js
+++ scripts/generateAndCheckSBOM.js
@@ -571,8 +571,7 @@ async function main() {
     await run('mvn -ntp -B org.cyclonedx:cyclonedx-maven-plugin:makeAggregateBom -q');
     await run('npm ls --depth 6', { output: 'target/tree-npm.txt' });
     await run('npm install --silent');
-    await run('npm install --silent --save-dev @cyclonedx/cyclonedx-npm');
-    await run('npx @cyclonedx/cyclonedx-npm --output-file target/bom-npm.json --output-format JSON');
+    await run('npx --yes @cyclonedx/cyclonedx-npm --output-file target/bom-npm.json --output-format JSON');
   }
 
   log(`generating 'bom-vaadin.js'`);
