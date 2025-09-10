b0cbd4f51e0cce452c4ad973f8fa10f36af0d3de
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-09-10 09:35:01 +0300

    chore: fix prod bundle test after the recent theme changes (#8018)
    * chore: fix prod bundle test after the recent theme changes
    
    * fix frontend location
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

--- vaadin-prod-bundle/src/main/java/com/vaadin/prodbundle/FakeAppConf.java
+++ vaadin-prod-bundle/src/main/java/com/vaadin/prodbundle/FakeAppConf.java
@@ -5,9 +5,7 @@ import com.vaadin.flow.component.dnd.internal.DndUtil;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.LoadDependenciesOnStartup;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 
-@Theme("vaadin-prod-bundle")
 @PWA(name = "vaadin-prod-bundle", shortName = "vaadin-prod-bundle")
 @LoadDependenciesOnStartup(EagerView.class)
 @JsModule(DndUtil.DND_CONNECTOR)  
