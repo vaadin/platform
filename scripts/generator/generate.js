350c94d9b24950cd68d3d411a4bb52dbdfc9e732
Zhe Sun <zhe@vaadin.com>
2026-01-29 16:11:09 +0200

    use component version instead of platform version

--- scripts/generator/generate.js
+++ scripts/generator/generate.js
@@ -125,4 +125,5 @@ writer.writeProperty(versions, "flow", platformJavadocTemplatePomFileName, platf
 
 writer.writeProperty(versions, "flow", devBundleTemplateFileName, devBundlePomResultFileName);
 writer.writeProperty(versions, "flow", prodBundleTemplateFileName, prodBundlePomResultFileName);
-
+writer.writeMaven(versions, devBundleTemplateFileName, devBundlePomResultFileName);
+writer.writeMaven(versions, prodBundleTemplateFileName, prodBundlePomResultFileName);
