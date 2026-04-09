2c204713a9fad18b05ddfafde065166d751901f9
Zhe Sun <zhe@vaadin.com>
2026-04-09 12:44:15 +0300

    chore: get rid of theme folder warning

--- vaadin-hybrid-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ vaadin-hybrid-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -1,10 +1,12 @@
 package com.vaadin.devbundle;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
-@Theme("vaadin-dev-bundle")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
 @PWA(name = "vaadin-dev-bundle", shortName = "vaadin-dev-bundle")
 public class FakeAppConf implements AppShellConfigurator{
 
